acf6dbaf44802326c41277127e1cf9114025c2e0
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2024-03-16 16:19:46 +0100

    to cjs and not full path

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
     name: "bunhook",
     script: "index.ts", // Entry point of your application
-    interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+    interpreter: "bun",
 };
